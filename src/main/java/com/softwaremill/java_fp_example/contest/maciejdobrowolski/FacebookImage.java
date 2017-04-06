d07a7e9ddd15522196fd567b14effad50619f59c
Maciej Dobrowolski <maciej.dobrowolski@4finance.com>
2017-04-06 16:13:45 +0200

    oh my god!

--- src/main/java/com/softwaremill/java_fp_example/contest/maciejdobrowolski/FacebookImage.java
+++ src/main/java/com/softwaremill/java_fp_example/contest/maciejdobrowolski/FacebookImage.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class FacebookImage {
 
     private final static String FACEBOOK_IMAGE_TAG = "og:image";
+    private final static MetaEntry FALLBACK_META_ENTRY = new MetaEntry(FACEBOOK_IMAGE_TAG, DEFAULT_IMAGE);
     private final static int TIMEOUT_IN_SECONDS = 10;
 
     private final String url;
@@ -39,11 +40,7 @@ public class FacebookImage {
     private ObservableTransformer<MetaEntry, MetaEntry> defaultOnTimeout() {
         return upstream -> upstream.timeout(TIMEOUT_IN_SECONDS, SECONDS)
                 .doOnError(e -> log.error("Unable to extract og:image from url {}. Problem: {}", url, e.getMessage()))
-                .onErrorReturn(e -> defaultEntry());
-    }
-
-    private static MetaEntry defaultEntry() {
-        return new MetaEntry(FACEBOOK_IMAGE_TAG, DEFAULT_IMAGE);
+                .onErrorReturnItem(FALLBACK_META_ENTRY);
     }
 
     private static boolean isFacebookImageTag(MetaEntry metaEntry) {
@@ -53,7 +50,7 @@ public class FacebookImage {
     private ObservableTransformer<MetaEntry, MetaEntry> firstOrDefault() {
         return upstream -> upstream.firstOrError()
                 .doOnError(e -> log.warn("No {} found for blog post {}", FACEBOOK_IMAGE_TAG, url))
-                .onErrorReturn(e -> defaultEntry())
+                .onErrorReturnItem(FALLBACK_META_ENTRY)
                 .toObservable();
 
     }
