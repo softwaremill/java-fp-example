1192d392a5293a0088e4c7d60b77b2c3c3a4b7c0
gcf <m36r1m@gmail.com>
2017-04-14 01:31:33 +0200

    remove junkfiles

--- src/main/java/com/softwaremill/java_fp_example/DefaultImage.java
+++
@@ -1,7 +0,0 @@
-package com.softwaremill.java_fp_example;
-
-public class DefaultImage {
-
-    public static final String DEFAULT_IMAGE = "https://softwaremill.com/images/logo-vertical.023d8496.png";
-    
-}
