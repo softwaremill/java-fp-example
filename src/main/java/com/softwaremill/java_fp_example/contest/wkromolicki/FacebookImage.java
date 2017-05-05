54442367217c7ff875d49a8ab90bd3d9b3cfa043
Wojciech Kromolicki <wojciech.kromolicki@4finance.com>
2017-05-05 15:06:36 +0200

    a little refactoring to better show the idea of buildng a program by composing smaller functions into bigger ones.

--- src/main/java/com/softwaremill/java_fp_example/contest/wkromolicki/FacebookImage.java
+++ src/main/java/com/softwaremill/java_fp_example/contest/wkromolicki/FacebookImage.java
@@ -15,13 +15,16 @@ public class FacebookImage {
 
     private final static Predicate<Element> FACEBOOK_IMAGE_TAG_FILTER = el -> "og:image".equals(el.attr("property"));
 
-
     public static String extractImage(String pageUrl) {
-        return imageFinder(FACEBOOK_IMAGE_TAG_FILTER)
+        return findImage.apply(FACEBOOK_IMAGE_TAG_FILTER)
                 .apply(pageUrl)
                 .onFailure(handleExceptionFor(pageUrl))
                 .getOrElse(DEFAULT_IMAGE);
     }
+
+    private static Function<Predicate<Element>, Function<String, Try<String>>> findImage =
+       filter -> fetchUrl.andThen(lift(parseUrl)).andThen(lift(findMetaTag.apply(filter))).andThen(lift(extractContent));
+
 
     private static Consumer<Throwable> handleExceptionFor(String pageUrl) {
         return e -> log(String.format("Unable to extract og:image from url %s. Problem: %s", pageUrl, e.getMessage()));
@@ -29,9 +32,5 @@ public class FacebookImage {
 
     private static void log(String message) {
         System.out.println(message);
-    }
-
-    private static Function<String, Try<String>> imageFinder(Predicate<Element> tagFilter) {
-        return parser.andThen(composeM(metaTagFinder(tagFilter))).andThen(composeM(contentExtractor));
     }
 }
--- src/main/java/com/softwaremill/java_fp_example/contest/wkromolicki/ParserFunctions.java
+++ src/main/java/com/softwaremill/java_fp_example/contest/wkromolicki/ParserFunctions.java
@@ -14,16 +14,25 @@ import javaslang.control.Try;
 public class ParserFunctions {
     public final static int TIMEOUT = 10_000;
 
-    public static Function<String, Try<Document>> parser = url -> Try.of(() -> Jsoup.parse(new URL(url), TIMEOUT));
+    public static Function<Predicate<Element>, Function<Document, Try<Element>>> findMetaTag =
+            tagFilter -> document -> List.ofAll(document.head().getElementsByTag("meta")).filter(tagFilter).headOption().toTry();
 
-    public static Function<Document, Try<Element>> metaTagFinder(Predicate<Element> tagFilter) {
 
-        return document -> List.ofAll(document.head().getElementsByTag("meta")).filter(tagFilter).headOption().toTry();
+    public static Function<Element, Try<String>> extractContent = safe(el -> el.attr("content"));
+
+    public static Function<String, Try<URL>> fetchUrl = safe(URL::new);
+
+    public static Function<URL, Try<Document>> parseUrl = safe(url -> Jsoup.parse(url, TIMEOUT));
+
+
+    //below are helper methods which do not belong to parser domain and should be in some library
+    public static <A,B> Function<Try<A>, Try<B>> lift(Function<A, Try<B>> func) {
+        return in -> in.flatMap(func::apply);
     }
 
-    public static Function<Element, Try<String>> contentExtractor = el -> Try.of(() -> el.attr("content"));
-
-    public static <A,B> Function<Try<A>, Try<B>> composeM(Function<A, Try<B>> composer) {
-        return in -> in.flatMap(composer::apply);
+    public static <A, B> Function<A, Try<B>> safe(Try.CheckedFunction<A, B> func) {
+        return a -> Try.of(() -> func.apply(a));
     }
+
+
 }
